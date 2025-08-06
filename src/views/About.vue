cbecf3163c55bf261609f9256abd501ca8175a22
Ricardo Rizk <ricardorizk2@gmail.com>
2025-08-06 10:45:09 -0400

    Import stuff

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,23 +6,9 @@
   </p>
   <p>I love coding, music, and biking and I enjoy playing with skill toys.</p>
 
-  <Carousel class="carousel" v-slot="{ currentSlide }">
-    <Slide v-for="(slide, index) in carouselSlides" :key="index">
-      <div v-show="currentSlide === index" class="slide-info">
-        <img :src="`/public/${slide}`" alt="Carousel Image" />
-      </div>
-    </Slide>
-  </Carousel>
 </template>
 
 <script setup>
-import Carousel from '@/components/Carousel.vue';
-import Slide from '@/components/Slide.vue';
-
-const carouselSlides = [
-  "image-c-1.jpg",
-  "image-c-2.jpg"
-];
 </script>
 
 <style lang="css" scoped>
@@ -35,29 +21,5 @@ p {
   max-width: 80%;
   margin: 1rem auto;
   text-align: center;
-}
-
-.carousel {
-  position: relative;
-  max-width: 80%;
-  margin: 2rem auto;
-}
-
-.slide-info {
-  position: relative;
-  width: 100%;
-  top: auto;
-  left: auto;
-  transform: none;
-  display: flex;
-  justify-content: center;
-}
-
-.slide-info img {
-
-  width: 100%;
-  height: auto;
-  max-height: 500px;
-  object-fit: contain;
 }
 </style>
