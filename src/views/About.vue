65b2288968ba0eab11bbe13605c2ccd811f0bea0
Ricardo Rizk <ricardorizk2@gmail.com>
2025-08-07 11:05:00 -0400

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
   <h1>About</h1>
-  <p>I was born in Dubai on the 13th of September, 2012. Now, I live in Canada with my family in Ontario. I am a
-    <i>mini</i> software developer and especially good at front end development. I also write music for my Youtube
-    channel and like learning new things
+  <p>I was born in Dubai in 2012. Now, I live in Ontario, Canada with my family. I am a
+    <i>mini</i> software developer and especially good at front end development. I also play music and like learning new
+    things.
   </p>
   <p>I love coding, music, and biking and I enjoy playing with skill toys.</p>
   <div class="swiper">
